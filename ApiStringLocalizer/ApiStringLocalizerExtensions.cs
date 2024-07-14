e915702aab8db7699f1d1b018422d912d99fea91
Ahmed Elbatal <abatal-c@nupco.com>
2024-07-14 11:03:55 +0300

    Fixing the DisableApiLocalization condition

--- ApiStringLocalizer/ApiStringLocalizerExtensions.cs
+++ ApiStringLocalizer/ApiStringLocalizerExtensions.cs
@@ -42,7 +42,7 @@ namespace Microsoft.Extensions.DependencyInjection
         }
         private static void AddApiStringLocalizerServices(IServiceCollection services, ApiStringLocalizerOptions options)
         {
-            if (options?.DisableApiLocalization != false)
+            if (options?.DisableApiLocalization != true)
             {
                 services.AddHttpClient();
                 services.AddMemoryCache();
